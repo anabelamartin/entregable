1ade051e5d419d4fe0483120ae5a754fc7f20af9
anabelamartin <anabelamartin@hotmail.com.ar>
2022-11-09 13:15:38 -0300

    subiendo cambios

--- consignas 3.0 - ESTUDIANTE/code.js
+++ consignas 3.0 - ESTUDIANTE/code.js
@@ -7,20 +7,20 @@ let datosPersona = {
 };
 
 const listado = [{
-    imgUrl: "https://huguidugui.files.wordpress.com/2015/03/html1.png",
-    lenguajes: "HTML y CSS",
-    bimestre: "1er bimestre",
-  },
-  {
-    imgUrl: "https://jherax.files.wordpress.com/2018/08/javascript_logo.png",
-    lenguajes: "Javascript",
-    bimestre: "2do bimestre",
-  },
-  {
-    imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png",
-    lenguajes: "React JS",
-    bimestre: "3er bimestre",
-  },
+  imgUrl: "https://huguidugui.files.wordpress.com/2015/03/html1.png",
+  lenguajes: "HTML y CSS",
+  bimestre: "1er bimestre",
+},
+{
+  imgUrl: "https://jherax.files.wordpress.com/2018/08/javascript_logo.png",
+  lenguajes: "Javascript",
+  bimestre: "2do bimestre",
+},
+{
+  imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png",
+  lenguajes: "React JS",
+  bimestre: "3er bimestre",
+},
 ];
 
 const profileBtn = document.querySelector("#completar-perfil");
@@ -35,23 +35,16 @@ cambiarTema.addEventListener("click", alternarColorTema);
 
 function obtenerDatosDelUsuario() {
   /* --------------- PUNTO 1: Escribe tu codigo a partir de aqui --------------- */
-  let nombre = '';
-  let edad = 0;
-  let ciudad='';
-  let interesPorJs='';
-  do{
-    nombre = prompt('Ingresa tu nombre:');
-    edad = 2022 - parseInt(prompt('Ingresa el año en que naciste: '));
-    ciudad = prompt("En que ciudad vivis: ");
-    interesPorJs = (confirm('te interesa javascript?')) ? 'Si' : 'No';
+  do {
+    datosPersona.nombre = prompt('Ingrese su nombre:')
+  } while (datosPersona.nombre === null || datosPersona.nombre === '' || datosPersona.nombre.length < 3 || !isNaN(datosPersona.nombre));
+  do { datosPersona.edad = 2022 - (prompt('Ingrese el año en que nacio: ')); }
+  while (datosPersona.edad === null || datosPersona.edad === '' || !Number(datosPersona.edad));
+  do {
+    datosPersona.ciudad = prompt('Ingrese la ciudad donde vive: ')
+  } while (datosPersona.ciudad === null || datosPersona.ciudad === '' || datosPersona.ciudad.length < 3 || !isNaN(datosPersona.ciudad));
 
-  }while( nombre === null || nombre === '' || nombre.length < 3 ||  !isNaN(nombre) );
-        (edad === null || edad==='' || edad.length<3 || Number.isInteger(edad));
-        ( ciudad === null || ciudad === '' || ciudad.length < 3 ||  !isNaN(ciudad) );
-
-  
- datosPersona = {nombre, edad, ciudad, interesPorJs}
-
+  datosPersona.interesPorJs = (confirm('Le interesa JavaScript?')) ? 'Si' : 'No';
 
 }
 
@@ -59,45 +52,48 @@ function renderizarDatosUsuario() {
   /* ------------------- NO TOCAR NI ELIMINAR ESTA FUNCION. ------------------- */
   obtenerDatosDelUsuario();
   /* --------------- PUNTO 2: Escribe tu codigo a partir de aqui --------------- */
-  let usuarioNombre = document.querySelector('#nombre');
-  usuarioNombre.innerText = datosPersona.nombre;
-  let usuarioEdad = document.querySelector('#edad');
-  usuarioEdad.innerText = datosPersona.edad;
-  let usuarioCiudad = document.querySelector('#ciudad');
-  usuarioCiudad.innerText = datosPersona.ciudad;
-  let usuarioJs = document.querySelector('#javascript');
-  usuarioJs.innerText = datosPersona.interesPorJs;  
-
+  let spanNombre = document.querySelector('#nombre');
+  spanNombre.innerText = datosPersona.nombre;
+  let spanEdad = document.querySelector('#edad');
+  spanEdad.innerText = datosPersona.edad;
+  let spanCiudad = document.querySelector('#ciudad');
+  spanCiudad.innerText = datosPersona.ciudad;
+  let spanJs = document.querySelector('#javascript');
+  spanJs.innerText = datosPersona.interesPorJs;
 
 }
 
 
 function recorrerListadoYRenderizarTarjetas() {
   /* ------------------ PUNTO 3: Escribe tu codigo desde aqui ------------------ */
- let html = '';
- listado.forEach(tarjetas => {
-  html +=
-  `<div class="caja">
+  const fila = document.querySelector("#fila")
+
+  if (document.querySelector('.caja')) return
+  let html = '';
+  listado.forEach(tarjetas => {
+    html +=
+      `<div class="caja">
        <img src= "${tarjetas.imgUrl}" alt=${tarjetas.lenguajes}">
        <p> Lenguajes:  ${tarjetas.lenguajes} </p>
        <p> Bimestre:  ${tarjetas.bimestre} </p>
   </div>`
-  
- }); 
- let fila = document.querySelector("#fila")
+
+  });
   fila.innerHTML = html;
-
-
-
 }
 
 function alternarColorTema() {
   /* --------------------- PUNTO 4: Escribe tu codigo aqui --------------------- */
- 
+sitio.classList.toggle('dark');
   
-
-
 }
 
 /* --------------------- PUNTO 5: Escribe tu codigo aqui --------------------- */
+let visualizar = document.querySelector("#sobre-mi")
+document.addEventListener('keydown',(e) =>{
+  let tecla = e.key;
+  if(tecla.toUpperCase() == 'F'){
+    visualizar.classList.toggle('oculto')
+  }
+})
 
